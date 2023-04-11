027d91eefa720ce22a0a6ff31a42a72bb6528b80
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2023-04-10 20:39:15 -0600

    Update Rtos_Test.ino

--- Tests/Rtos/Rtos_Test.ino
+++ Tests/Rtos/Rtos_Test.ino
@@ -1,17 +1,9 @@
-#define LED_PIN 13 
-
 void setup() {
-  Serial.begin(9600); 
-  pinMode(LED_PIN, OUTPUT); 
+
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); 
-  Serial.println("LED turned ON."); 
-  delay(1000); 
-  digitalWrite(LED_PIN, LOW); 
-  Serial.println("LED turned OFF."); 
-  delay(1000); 
+
 }
 
 
