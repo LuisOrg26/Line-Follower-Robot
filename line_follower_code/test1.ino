2390d4305f4c3cad0dbec7865d3dfe5a6a811230
danvaz313 <danisantos313sv@gmail.com>
2023-04-10 19:46:33 -0600

    Update test1.ino

--- line_follower_code/test1.ino
+++ line_follower_code/test1.ino
@@ -1,13 +1,18 @@
 #define LED 22
 
 void setup() {
+    Serial.begin(9600);
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
   digitalWrite(LED, HIGH);
+  Serial.print("Encendido");
+
   delay(500);
   digitalWrite(LED, LOW);
+  Serial.print("Apagado");
+
   delay(500);
 }
 
